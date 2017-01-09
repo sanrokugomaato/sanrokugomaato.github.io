49a4fb3c462e76317d3d12d0af3441e47ae37577
Jun <me@noraesae.net>
2017-01-09 17:45:14 +0900

    Update index page

--- main.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  function changeBgColor() {
-    var color = randomColor({ luminosity: 'light' });
-    document.body.style.backgroundColor = color;
-  }
-
-  setInterval(changeBgColor, 7000);
-})();
